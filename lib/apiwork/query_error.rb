3845102109c1127c264d8160292876021bc63945
joauh <joakimuhrwing@gmail.com>
2025-11-14 10:39:18 +0100

    Checkpoint

--- lib/apiwork/query_error.rb
+++ lib/apiwork/query_error.rb
@@ -2,26 +2,5 @@
 
 module Apiwork
   class QueryError < Error
-    attr_reader :code, :path, :meta
-
-    def initialize(code:, message:, path: [], meta: {})
-      @code = code
-      @path = Array(path).map { |element| element.is_a?(Integer) ? element : element.to_sym }
-      @meta = meta
-      super(message)
-    end
-
-    def pointer
-      @pointer ||= Errors::JSONPointer.build(*path)
-    end
-
-    def to_h
-      {
-        code: code,
-        path: path.map(&:to_s),
-        pointer: pointer,
-        detail: message
-      }
-    end
   end
 end
--- spec/dummy/app/controllers/api/v1/v1_controller.rb
+++ spec/dummy/app/controllers/api/v1/v1_controller.rb
@@ -5,14 +5,6 @@ module Api
     class V1Controller < ApplicationController
       include Apiwork::Controller::Concern
 
-      rescue_from Apiwork::ConstraintErrorCollection do |error|
-        render json: { ok: false, errors: error.to_array }, status: :bad_request
-      end
-
-      rescue_from Apiwork::ConstraintError, Apiwork::QueryError, Apiwork::SchemaError do |error|
-        render json: { ok: false, errors: [error.to_h] }, status: :bad_request
-      end
-
       rescue_from ActiveRecord::RecordNotFound do |error|
         render json: { ok: false, errors: [{ code: 'not_found', detail: error.message }] }, status: :not_found
       end
