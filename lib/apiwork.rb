16027f42337132ce07a5277af35ee68a67c41c38
joauh <joakimuhrwing@gmail.com>
2025-11-22 07:22:01 +0100

    Checkpoint

--- lib/apiwork.rb
+++ lib/apiwork.rb
@@ -5,10 +5,12 @@ require_relative 'apiwork/version'
 
 module Apiwork
   class << self
+    # DOCUMENTATION
     def routes
       @routes ||= API::RackApp.new
     end
 
+    # DOCUMENTATION
     def reset_registries!
       API::Registry.clear!
       Contract::SchemaRegistry.clear!
@@ -23,7 +25,6 @@ loader.inflector.inflect(
   'api' => 'API',
   'json_pointer' => 'JSONPointer'
 )
-
 loader.setup
 
 require_relative 'apiwork/engine' if defined?(Rails::Engine)
