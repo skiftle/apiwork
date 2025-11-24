494552db802a2c51825cbfb3beb2b6907ad5b7f0
joauh <joakimuhrwing@gmail.com>
2025-11-24 10:55:49 +0100

    Checkpoint

--- lib/apiwork/adapter/base.rb
+++ lib/apiwork/adapter/base.rb
@@ -4,31 +4,31 @@ module Apiwork
   module Adapter
     class Base
       def build_global_descriptors(builder)
-        raise NotImplementedError, "#{self.class}#build_global_descriptors must be implemented"
+        raise NotImplementedError
       end
 
       def build_contract(contract_class, schema_class, context)
-        raise NotImplementedError, "#{self.class}#build_contract must be implemented"
+        raise NotImplementedError
       end
 
       def collection_scope(collection, schema_class, query, context)
-        raise NotImplementedError, "#{self.class}#collection_scope must be implemented"
+        raise NotImplementedError
       end
 
       def record_scope(record, schema_class, query, context)
-        raise NotImplementedError, "#{self.class}#record_scope must be implemented"
+        raise NotImplementedError
       end
 
       def render_collection(collection, meta, query, schema_class, context)
-        raise NotImplementedError, "#{self.class}#render_collection must be implemented"
+        raise NotImplementedError
       end
 
       def render_record(record, meta, query, schema_class, context)
-        raise NotImplementedError, "#{self.class}#render_record must be implemented"
+        raise NotImplementedError
       end
 
       def render_error(issues, context)
-        raise NotImplementedError, "#{self.class}#render_error must be implemented"
+        raise NotImplementedError
       end
     end
   end
