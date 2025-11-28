20ae884fd7951f821bcdf2144f1fe96e474b2307
joauh <joakimuhrwing@gmail.com>
2025-11-28 14:50:46 +0100

    Checkpoints

--- lib/apiwork/controller/deserialization.rb
+++ lib/apiwork/controller/deserialization.rb
@@ -6,49 +6,35 @@ module Apiwork
       extend ActiveSupport::Concern
 
       included do
-        before_action :validate_input
-      end
-
-      class_methods do
-        def skip_validate_input!(only: nil, except: nil)
-          skip_before_action :validate_input, only: only, except: except
-        end
+        before_action :validate_contract
       end
 
       def contract
         @contract ||= contract_class.new(
-          query: transformed_query_params,
-          body: transformed_body_params,
+          query: transformed_query_parameters,
+          body: transformed_body_parameters,
           action: action_name
         )
       end
 
-      def query
-        contract.query
-      end
-
-      def body
-        contract.body
-      end
-
       private
 
-      def validate_input
+      def validate_contract
         return if contract.valid?
 
         raise ContractError, contract.issues
       end
 
-      def transformed_query_params
-        data = request.query_parameters.deep_symbolize_keys
+      def transformed_query_parameters
+        parameters = request.query_parameters.deep_symbolize_keys
         schema_class = contract_class.action_definition(action_name)&.schema_class
-        schema_class ? adapter.transform_request(data, schema_class) : data
+        schema_class ? adapter.transform_request(parameters, schema_class) : parameters
       end
 
-      def transformed_body_params
-        data = request.request_parameters.deep_symbolize_keys
+      def transformed_body_parameters
+        parameters = request.request_parameters.deep_symbolize_keys
         schema_class = contract_class.action_definition(action_name)&.schema_class
-        schema_class ? adapter.transform_request(data, schema_class) : data
+        schema_class ? adapter.transform_request(parameters, schema_class) : parameters
       end
     end
   end
