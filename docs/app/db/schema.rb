c8242f8cc14d166be06c1cd791a71069d6c265d1
joauh <joakimuhrwing@gmail.com>
2025-11-30 10:00:24 +0100

    Checkpoint

--- docs/app/db/schema.rb
+++ docs/app/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,32 +12,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_30_000004) do
-  create_table "eager_lion_customers", id: :string, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "name", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[8.1].define(version: 20_251_130_000_004) do
+  create_table 'eager_lion_customers', id: :string, force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.string 'name', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "eager_lion_invoices", id: :string, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "customer_id", null: false
-    t.date "issued_on"
-    t.string "notes"
-    t.string "number", null: false
-    t.string "status"
-    t.datetime "updated_at", null: false
+  create_table 'eager_lion_invoices', id: :string, force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.string 'customer_id', null: false
+    t.date 'issued_on'
+    t.string 'notes'
+    t.string 'number', null: false
+    t.string 'status'
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "eager_lion_lines", id: :string, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "description"
-    t.string "invoice_id", null: false
-    t.decimal "price", precision: 10, scale: 2
-    t.integer "quantity"
-    t.datetime "updated_at", null: false
+  create_table 'eager_lion_lines', id: :string, force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.string 'description'
+    t.string 'invoice_id', null: false
+    t.decimal 'price', precision: 10, scale: 2
+    t.integer 'quantity'
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "eager_lion_invoices", "eager_lion_customers", column: "customer_id"
-  add_foreign_key "eager_lion_lines", "eager_lion_invoices", column: "invoice_id"
+  add_foreign_key 'eager_lion_invoices', 'eager_lion_customers', column: 'customer_id'
+  add_foreign_key 'eager_lion_lines', 'eager_lion_invoices', column: 'invoice_id'
 end
