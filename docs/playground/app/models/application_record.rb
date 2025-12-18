a4e631855a0fe646cfbf44d639e6f6b42a2f32de
joauh <joakimuhrwing@gmail.com>
2025-12-18 15:00:01 +0100

    Checkpoint

--- docs/playground/app/models/application_record.rb
+++ docs/playground/app/models/application_record.rb
@@ -3,22 +3,28 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  before_create :set_uuid
+  before_create :set_id
 
-  def self.inherited(subclass)
-    super
-    return unless subclass.name&.include?('::')
+  class << self
+    def generate_uuid
+      @seq = (@seq || 0) + 1
+      Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, "#{table_name}:#{@seq}")
+    end
 
-    return if subclass.superclass != ApplicationRecord && subclass.superclass.name&.include?('::')
+    def inherited(subclass)
+      super
+      return if subclass.name&.exclude?('::')
+      return unless subclass.superclass.abstract_class?
 
-    namespace = subclass.name.deconstantize.underscore.tr('/', '_')
-    model_name = subclass.name.demodulize.underscore.pluralize
-    subclass.table_name = "#{namespace}_#{model_name}"
+      namespace = subclass.name.deconstantize.underscore.tr('/', '_')
+      model_name = subclass.name.demodulize.underscore.pluralize
+      subclass.table_name = "#{namespace}_#{model_name}"
+    end
   end
 
   private
 
-  def set_uuid
-    self.id ||= SecureRandom.uuid
+  def set_id
+    self.id ||= self.class.generate_uuid
   end
 end
