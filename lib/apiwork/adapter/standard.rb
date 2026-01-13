a4f8a09fae6ad9fabf465f9dba9b8a3a7da9a69b
joauh <joakimuhrwing@gmail.com>
2026-01-13 11:09:22 +0100

    Checkpoint

--- lib/apiwork/adapter/standard.rb
+++ lib/apiwork/adapter/standard.rb
@@ -21,17 +21,18 @@ module Apiwork
 
       def render_collection(collection, schema_class, state)
         CollectionLoader.load(collection, schema_class, state) => { data:, metadata: }
-        serialized = schema_class.serialize(data, context: state.context, include: state.query[:include])
+        data = schema_class.serialize(data, context: state.context, include: state.query[:include])
 
         {
-          schema_class.root_key.plural => serialized,
+          schema_class.root_key.plural => data,
           pagination: metadata[:pagination],
           meta: state.meta.presence,
         }.compact
       end
 
       def render_record(record, schema_class, state)
-        RecordValidator.validate(record, schema_class)
+        RecordValidator.validate!(record, schema_class)
+
         data = RecordLoader.load(record, schema_class, state.query)
         data = schema_class.serialize(data, context: state.context, include: state.query[:include])
 
--- lib/apiwork/adapter/standard/record_validator.rb
+++ lib/apiwork/adapter/standard/record_validator.rb
@@ -4,18 +4,16 @@ module Apiwork
   module Adapter
     class Standard
       class RecordValidator
-        attr_reader :schema_class
-
-        def self.validate(record, schema_class)
-          new(record, schema_class).validate
+        def self.validate!(record, schema_class)
+          new(record, schema_class).validate!
         end
 
-        def initialize(record, schema_class = nil)
+        def initialize(record, schema_class)
           @record = record
           @schema_class = schema_class
         end
 
-        def validate
+        def validate!
           return unless @record.respond_to?(:errors) && @record.errors.any?
 
           raise DomainError, issues
@@ -23,16 +21,8 @@ module Apiwork
 
         def issues
           locale_key = @schema_class.api_class.structure.locale_key
-          DomainIssueMapper.call(@record, locale_key:, root_path: root_path)
-        end
-
-        private
-
-        def root_path
-          return [:data] unless @schema_class
-
-          type_key = @schema_class.root_key.singular
-          [type_key.to_sym]
+          root_path = [@schema_class.root_key.singular.to_sym]
+          DomainIssueMapper.call(@record, locale_key:, root_path:)
         end
       end
     end
